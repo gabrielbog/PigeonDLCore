4fb9080a6fbb8356ec529b0b4b939f38322c12bd
gabrielbog <bogoslovgabriel@gmail.com>
2024-03-04 20:28:49 +0200

    Implemented Dark Mode toggle
    Updated Bootstrap to 5.3.3, added dropdowns

--- PigeonDLCore/Program.cs
+++ PigeonDLCore/Program.cs
@@ -5,6 +5,8 @@ using PigeonDLCore.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
---
+++ PigeonDLCore/wwwroot/js/scripts/layout/generic.js
@@ -0,0 +1,63 @@
+﻿function getCurrentThemeString() {
+    const themeSetting = localStorage.getItem("themeSetting");
+    if (themeSetting == "light") {
+        return "Theme: Light";
+    }
+    else if (themeSetting == "dark") {
+        return "Theme: Dark";
+    }
+}
+
+function setLightTheme() {
+    localStorage.setItem("themeSetting", "light");
+    document.documentElement.setAttribute("data-bs-theme", "light");
+}
+
+function setDarkTheme() {
+    localStorage.setItem("themeSetting", "dark");
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+}
+
+function setDefaultTheme() {
+    const themeSetting = localStorage.getItem("themeSetting");
+
+    if (themeSetting == "light") {
+        setLightTheme();
+    }
+    else if (themeSetting == "dark") {
+        setDarkTheme();
+    }
+    else {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            setLightTheme();
+        }
+        else {
+            setDarkTheme();
+        }
+    }
+}
+
+setDefaultTheme();
+
+window.addEventListener("DOMContentLoaded", function () {
+
+    const settingsDropdown = document.getElementById("settings-dropdown");
+    settingsDropdown.addEventListener("show.bs.dropdown", function () {
+
+        const currentThemeSetting = localStorage.getItem("themeSetting");
+        var themeToggle = document.getElementById("theme-toggle");
+        themeToggle.innerHTML = getCurrentThemeString();
+
+        themeToggle.addEventListener("click", function () {
+
+            if (currentThemeSetting == "dark") {
+                setLightTheme();
+                themeToggle.innerHTML = getCurrentThemeString();
+            }
+            else if (currentThemeSetting == "light") {
+                setDarkTheme();
+                themeToggle.innerHTML = getCurrentThemeString();
+            }
+        });
+    });
+});
